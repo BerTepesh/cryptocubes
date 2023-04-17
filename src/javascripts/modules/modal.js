968d903cb0495a989164f9ed06d5161e5630f7b9
Alexey Berezin <berezin.ext@arrival.com>
2023-04-17 16:42:13 +0300

    Initial commit

---
+++ src/javascripts/modules/modal.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+
+Fancybox.bind('.side-modal-trigger', {
+  mainClass: "side-modal",
+	groupAttr: false,
+	dragToClose: false
+});
+
